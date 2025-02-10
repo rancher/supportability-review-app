5ce8bad087a729f0b77a63130797ab50c28ca4bf
piyushkrana1 <piyush.rana@suse.com>
2025-02-10 11:42:28 +0530

    New dynamic Ui initial commit

--- pkg/supportability-review-app/components/ReportView.vue
+++ pkg/supportability-review-app/components/ReportView.vue
@@ -1,55 +1,71 @@
 <template>
-  <div>
-    <!-- Title page -->
-    <div ref="printableReport" class="sr-letter-size">
-      <ClusterDistroWidget :info="clusterData" />
-      <ChecksSummaryWidget :info="summaryData" />
-      <VectorsInfoWidgetDoughnut :info="vectorData" />
-      <Summary />
-    </div>
+  <div class="layout">
+    <Sidebar class="sidebar" :clusterData="clusterData" :summaryData="summaryData" />
+    <Maincontent class="main-content" :vectorData="vectorData" :cards="cards" />
   </div>
 </template>
-
 <script>
-import PrimeLogo from '../images/rancher-prime-logo-color-720x290.jpg';
-import ClusterDistroWidget from './ClusterDistroWidget.vue';
-import ChecksSummaryWidget from './ChecksSummaryWidget.vue';
-import VectorsInfoWidgetDoughnut from './VectorsInfoWidgetDoughnut.vue';
-import Summary from './Summary.vue';
-
+import Sidebar from './Sidebar.vue';
+import Maincontent from './Maincontent.vue';
 export default {
+  components: { Sidebar, Maincontent },
   name: 'ReportView',
-  components: {
-    ClusterDistroWidget,
-    ChecksSummaryWidget,
-    VectorsInfoWidgetDoughnut,
-    Summary
-  },
-  setup() {
-    return {
-      PrimeLogo
-    };
-  },
   data() {
     return {
+      cards: [
+        {
+          id: 1,
+          heading: 'Sample Card 1',
+          details: 'Ensure kubelet is not affected by CVE-2024-10220'
+        },
+        {
+          id: 2,
+          heading: 'Sample Card 2',
+          details: 'Ensure cluster has minimum two (2) nodes.'
+        },
+        {
+          id: 3,
+          heading: 'Sample Card 3',
+          details: 'Ensure cluster has minimum two (2) controlplane nodes.'
+        }
+      ],
       reportObject: null,
       clusterData: [],
       summaryData: {
         checks_total: 0,
         checks_fail: 0,
         checks_warn: 0,
-        checks_skip: 0,
         checks_pass: 0
       },
-      vectorData: new Map()
+      vectorData: new Map(),
+      circleRadius: 45
     };
   },
-  created() {
+  mounted() {
     this.fetchReport();
   },
-
+  computed: {
+    circleCircumference() {
+      return 2 * Math.PI * this.circleRadius;
+    },
+    strokeDashoffset() {
+      return this.circleCircumference * (1 - this.progressPercentage / 100);
+    },
+    progressPassWidth() {
+      return (this.summaryData.checks_pass / this.summaryData.checks_total) * 100;
+    },
+    progressFailWidth() {
+      return (this.summaryData.checks_fail / this.summaryData.checks_total) * 100;
+    },
+    progressWarnWidth() {
+      return (this.summaryData.checks_warn / this.summaryData.checks_total) * 100;
+    },
+    progressPercentage() {
+      return ((this.summaryData.checks_pass / this.summaryData.checks_total) * 100).toFixed(1);
+    }
+  },
   methods: {
-    fetchReport() {
+    async fetchReport() {
       const name = this.$route.query.bundlename;
       const hostname = window.location.host;
       const path =
@@ -59,95 +75,90 @@ export default {
         '?key=' +
         name +
         '&type=data';
-      fetch(path)
-        .then((response) => response.json())
-        .then((reportData) => {
-          const report_data = {
-            clusterData: [
-              { type: 'aks', count: 0 },
-              { type: 'eks', count: 0 },
-              { type: 'gke', count: 0 },
-              { type: 'harvester', count: 0 },
-              { type: 'k3s', count: 0 },
-              { type: 'rke', count: 0 },
-              { type: 'rke2', count: 0 }
-            ],
-            summaryData: {
-              checks_total: 0,
-              checks_fail: 0,
-              checks_warn: 0,
-              checks_skip: 0,
-              checks_pass: 0
-            },
-            vectorData: {}
-          };
 
-          reportData.clusters.forEach((cluster) => {
-            const typeIndex = report_data.clusterData.findIndex((c) => c.type === cluster.kubernetes_distro);
-            if (typeIndex !== -1) {
-              report_data.clusterData[typeIndex].count++;
-            }
-            report_data.summaryData.checks_total += cluster.checks_total;
-            report_data.summaryData.checks_fail += cluster.checks_fail;
-            report_data.summaryData.checks_warn += cluster.checks_warn;
-            report_data.summaryData.checks_skip += cluster.checks_skip;
-            report_data.summaryData.checks_pass += cluster.checks_pass;
-            cluster.groups.forEach((group) => {
-              group.checks.forEach((check) => {
-                if (!report_data.vectorData[check.vector]) {
-                  report_data.vectorData[check.vector] = {
-                    checks_total: 0,
-                    checks_pass: 0,
-                    checks_fail: 0,
-                    checks_warn: 0,
-                    checks_skip: 0
-                  };
-                }
-                report_data.vectorData[check.vector].checks_pass += check.state === 'pass';
-                report_data.vectorData[check.vector].checks_fail += check.state === 'fail';
-                report_data.vectorData[check.vector].checks_skip += check.state === 'skip';
-                report_data.vectorData[check.vector].checks_warn += check.state === 'warn';
-              });
+      try {
+        const response = await fetch(path);
+        const reportData = await response.json();
+        const report_data = {
+          clusterData: [
+            { type: 'aks', count: 0 },
+            { type: 'eks', count: 0 },
+            { type: 'gke', count: 0 },
+            { type: 'harvester', count: 0 },
+            { type: 'k3s', count: 0 },
+            { type: 'rke', count: 0 },
+            { type: 'rke2', count: 0 }
+          ],
+          summaryData: {
+            checks_total: 0,
+            checks_fail: 0,
+            checks_warn: 0,
+            checks_pass: 0,
+            checks_skip: 0
+          },
+          vectorData: {}
+        };
+
+        reportData.clusters.forEach((cluster) => {
+          const typeIndex = report_data.clusterData.findIndex((c) => c.type === cluster.kubernetes_distro);
+          if (typeIndex !== -1) {
+            report_data.clusterData[typeIndex].count++;
+          }
+
+          report_data.summaryData.checks_fail += cluster.checks_fail;
+          report_data.summaryData.checks_warn += cluster.checks_warn;
+          report_data.summaryData.checks_pass += cluster.checks_pass;
+          report_data.summaryData.checks_skip += cluster.checks_skip;
+          report_data.summaryData.checks_total += cluster.checks_total - cluster.checks_skip;
+          cluster.groups.forEach((group) => {
+            group.checks.forEach((check) => {
+              if (!report_data.vectorData[check.vector]) {
+                report_data.vectorData[check.vector] = {
+                  checks_total: 0,
+                  checks_pass: 0,
+                  checks_fail: 0,
+                  checks_warn: 0
+                };
+              }
+              report_data.vectorData[check.vector].checks_pass += check.state === 'pass';
+              report_data.vectorData[check.vector].checks_fail += check.state === 'fail';
+              report_data.vectorData[check.vector].checks_warn += check.state === 'warn';
             });
-            for (const vectorName in report_data.vectorData) {
-              const vectorData = report_data.vectorData[vectorName];
-              vectorData.checks_total =
-                vectorData.checks_pass + vectorData.checks_fail + vectorData.checks_warn + vectorData.checks_skip;
-            }
           });
-          this.clusterData = report_data.clusterData || [];
-          this.summaryData = report_data.summaryData || {};
-          this.vectorData = new Map(Object.entries(report_data.vectorData || {}));
-        })
-        .catch(console.error);
+          for (const vectorName in report_data.vectorData) {
+            const vectorData = report_data.vectorData[vectorName];
+            vectorData.checks_total = vectorData.checks_pass + vectorData.checks_fail + vectorData.checks_warn;
+          }
+        });
+        this.clusterData = report_data.clusterData || [];
+        this.summaryData = report_data.summaryData || {};
+        this.vectorData = new Map(Object.entries(report_data.vectorData || {}));
+      } catch (error) {
+        console.log('Report Error');
+      }
     }
   }
 };
 </script>
 
-<style scoped lang="scss">
-.button-container {
+<style scoped>
+.layout {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 60px;
-}
-.custom-button {
-  background-color: #2453ff;
-  color: white;
-  padding: 6px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-}
-.custom-button:hover {
-  background-color: #0056b3;
+  height: 88vh;
+  background: grey;
+  overflow: hidden;
+  border: 0px solid green;
 }
 
-.sr-letter-size {
-  background-color: rgb(255, 255, 255);
-  width: 8.5in;
-  height: 11in;
+.sidebar {
+  flex: 0 0 25%;
+  color: white;
+  background-color: #2e3e50;
+  text-align: center;
+  border: 0px solid black;
+}
+
+.main-content {
+  flex-grow: 1;
 }
 </style>
