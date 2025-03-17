0bdd48210c51fa4e3b8562b5811ca8e210180277
Masashi Honma <masashi.honma@gmail.com>
2025-03-17 14:00:04 +0900

    Disable lints
    Signed-off-by: Masashi Honma <masashi.honma@gmail.com>

--- pkg/supportability-review-app/eslint.config.mjs
+++ pkg/supportability-review-app/eslint.config.mjs
@@ -23,13 +23,13 @@ export default [
       // JavaScript-specific rules
       'no-console': 'off',
       'no-unused-vars': 'warn',
-      'no-undef': 'warn',
+      'no-undef': 'off',
       'no-var': 'error',
       'prefer-const': 'error',
 
       // TypeScript-specific rules
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-require-imports': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-expressions': 'warn',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-non-null-assertion': 'error',
@@ -46,20 +46,20 @@ export default [
     },
     rules: {
       // Vue-specific rules
-      'no-undef': 'warn',
+      'no-undef': 'off',
       'no-unused-vars': 'error',
       'no-var': 'error',
       'prefer-const': 'error',
 
       '@typescript-eslint/no-unused-vars': 'error',
-      'vue/attribute-hyphenation': 'warn',
+      'vue/attribute-hyphenation': 'off',
       'vue/attributes-order': 'error',
       'vue/html-closing-bracket-newline': 'off',
       'vue/html-indent': 'off',
       'vue/html-self-closing': 'off',
       'vue/max-attributes-per-line': 'off',
       'vue/multi-word-component-names': 'error',
-      'vue/no-mutating-props': 'warn',
+      'vue/no-mutating-props': 'off',
       'vue/no-required-prop-with-default': 'error',
       'vue/no-unused-vars': 'error',
       'vue/no-unused-components': 'error',
