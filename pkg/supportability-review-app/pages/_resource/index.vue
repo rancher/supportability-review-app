df3c890cb8c59b80e12198d1dd7234272c6746e2
Alexandre Alves <alexandre.alves@suse.com>
2026-03-27 10:57:07 +0000

    fix list view pattern

--- pkg/supportability-review-app/pages/_resource/index.vue
+++ pkg/supportability-review-app/pages/_resource/index.vue
@@ -1,21 +1,12 @@
 <script>
 import ResourceList from '@shell/components/ResourceList/index.vue';
-import { SUPPORTABILITY_REVIEW_CRD_IDS } from '../../config/types';
 
 export default {
   name: 'ListSupportabilityReviewBundleResource',
   components: { ResourceList },
-  data() {
-    return {};
-  },
-  async fetch() {
-    // needed to populate cluster name col on machine inventories list
-    await this.$store.dispatch(`management/findAll`, { type: SUPPORTABILITY_REVIEW_CRD_IDS.REVIEW_BUNDLE });
-  },
-  computed: {}
 };
 </script>
 
 <template>
-  <ResourceList :has-advanced-filtering="advancedFilteringEnabled"></ResourceList>
+  <ResourceList></ResourceList>
 </template>
