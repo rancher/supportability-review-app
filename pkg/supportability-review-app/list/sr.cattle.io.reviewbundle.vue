6ef53f55e6663587e61f14d67307e7d878286bc1
Alexandre Alves <alexandre.alves@suse.com>
2026-03-27 10:57:07 +0000

    fix list view pattern

---
+++ pkg/supportability-review-app/list/sr.cattle.io.reviewbundle.vue
@@ -0,0 +1,42 @@
+<script>
+import Loading from '@shell/components/Loading';
+import ResourceTable from '@shell/components/ResourceTable';
+
+import { SUPPORTABILITY_REVIEW_CRD_IDS } from '../config/types';
+
+export default {
+  name: 'SrCattleIoReviewBundle',
+  components: {
+    Loading,
+    ResourceTable
+  },
+  props: {
+    resource: {
+      type:     String,
+      required: true,
+    },
+    schema: {
+      type:     Object,
+      required: true,
+    },
+  },
+  async fetch() {
+    await this.$store.dispatch(`management/findAll`, { type: SUPPORTABILITY_REVIEW_CRD_IDS.REVIEW_BUNDLE });
+  },
+  computed: {
+    rows() {
+      return this.$store.getters['management/all'](SUPPORTABILITY_REVIEW_CRD_IDS.REVIEW_BUNDLE);
+    }
+  }
+};
+</script>
+
+<template>
+  <Loading v-if="$fetchState.pending" />
+  <div v-else>
+    <ResourceTable
+      :schema="schema"
+      :rows="rows"
+    />
+  </div>
+</template>
